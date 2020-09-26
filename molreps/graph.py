25d736a86426158d502bb06d97ffa11774a92fc7
Patrick Reiser <64005383+PatReis@users.noreply.github.com>
2020-09-26 16:37:16 +0200

    Add files via upload

---
+++ molreps/graph.py
@@ -0,0 +1,9 @@
+"""
+Main graph generator for making molecular graphs.
+
+It uses networkx as graph interface and a mol object from rdkit, ase, pymatgen or similar.
+
+@author: Patrick Reiser,
+"""
+
+
