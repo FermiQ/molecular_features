80f478f15f5e74b030cf498865ae07ca77858bb2
Patrick Reiser <64005383+PatReis@users.noreply.github.com>
2020-09-26 19:58:11 +0200

    Add files via upload

--- molreps/graph.py
+++ molreps/graph.py
@@ -33,7 +33,7 @@ class MolGraph(nx.Graph):
         
         # Main mol object to use.
         self.mol = None
-        
+            
     
     def mol_from_smiles(self,in_smile):
         """
@@ -61,20 +61,87 @@ class MolGraph(nx.Graph):
         pass
     
     
-    def _proton_atom(self,molgraph):
-        pass
-        
-    def _label_atom(self,molgraph):
+    def _make_state(self,key,propy,args):
         pass
     
-    def make(self,nodes={'proton' : {'class': "proton_atom" } ,
-                         'label' : {'class' : "label_atom" } ,
+    def _make_edges(self,key,propy,args):
+        if(propy=="bond_type"):
+            lenats = len(self.mol.GetAtoms())
+            out = np.zeros((lenats,lenats))
+            bonds = list(self.mol.GetBonds())
+            for x in bonds:
+                attr = {key : int(x.GetBondType())}
+                self.add_edge(x.GetBeginAtomIdx(), x.GetEndAtomIdx(), **attr) 
+                self.add_edge(x.GetEndAtomIdx(),x.GetBeginAtomIdx(), **attr) 
+
+        
+    def _make_nodes(self,key,propy,args):
+        if(propy=="proton"):
+            #lenats = len(self.mol.GetAtoms())
+            for i,atm in enumerate(self.mol.GetAtoms()):
+                attr = {key: atm.GetAtomicNum()}
+                self.add_node(i, **attr)
+    
+    def make(self,nodes={'proton' : "proton" ,
+                         'atom' : "atom_label"
                          },
-                  edges = {},
-                  state = {}):
-        pass
+                  edges = {'bond' : 'bond_type',
+                           'distance' : {'class' : 'distance' , 'args' : {'bonds_onyly' : True, 'max_distance' : np.inf , 'max_partners' : np.inf}}
+                           },
+                  state = {'size' : 'num_atoms' 
+                           }):
+        
+        for key,value in nodes.items():
+            if(isinstance(value,str) == True):
+                self._make_nodes(key,value,None)
+            elif(isinstance(value,dict) == True): 
+                if('class' not in value):
+                    raise ValueError(" 'class' method must be defined in",value)
+                if(isinstance(value['class'],str) == True):
+                    args = value['args'] if 'args' in value else None
+                    self._make_nodes(key,value['class'],args)
+                else:
+                    args = value['args'] if 'args' in value else {}
+                    value['class'](self,key,**args)
+            else:
+                raise TypeError("Method must be a dict of {'class' : callable function/class or identifier, 'args' : {'value' : 0} }, with optinal args but got", value, "instead")
+                
+        for key,value in edges.items():
+            if(isinstance(value,str) == True):
+                self._make_edges(key,value,None)
+            elif(isinstance(value,dict) == True): 
+                if('class' not in value):
+                    raise ValueError(" 'class' method must be defined in",value)
+                if(isinstance(value['class'],str) == True):
+                    args = value['args'] if 'args' in value else None
+                    self._make_edges(key,value['class'],args)
+                else:
+                    args = value['args'] if 'args' in value else {}
+                    value['class'](self,key,**args)
+            else:
+                raise TypeError("Method must be a dict of {'class' : callable function/class or identifier, 'args' : {'value' : 0} }, with optinal args but got", value, "instead")
+        
+        for key,value in state.items():
+            if(isinstance(value,str) == True):
+                self._make_state(key,value,None)
+            elif(isinstance(value,dict) == True): 
+                if('class' not in value):
+                    raise ValueError(" 'class' method must be defined in",value)
+                if(isinstance(value['class'],str) == True):
+                    args = value['args'] if 'args' in value else None
+                    self._make_state(key,value['class'],args)
+                else:
+                    args = value['args'] if 'args' in value else {}
+                    value['class'](self,key,**args)
+            else:
+                raise TypeError("Method must be a dict of {'class' : callable function/class or identifier, 'args' : {'value' : 0} }, with optinal args but got", value, "instead")
+            
     
     def to_tensor(self):
         pass
         
     
+test = MolGraph()
+test.mol_from_smiles("CCCO")
+test.make()
+nx.draw(test)
